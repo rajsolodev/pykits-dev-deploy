70f8698029114245edeecbd35b21031b921a3086
Rajesh <geekyshows@gmail.com>
2026-02-23 18:20:49 +0530

    fixed bug

--- install-ssl.sh
+++ install-ssl.sh
@@ -81,7 +81,7 @@ server {
         proxy_read_timeout 60s;
 
         proxy_http_version 1.1;
-        proxy_set_header Upgrade $http_upgrade;
+        proxy_set_header Upgrade \$http_upgrade;
         proxy_set_header Connection "upgrade";
     }
 }
